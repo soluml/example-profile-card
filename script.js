a3fa923909d545681cad4f04039a866990131df6
soluml <benjamin@soluml.com>
2024-08-23 11:46:34 -0500

    Update script

--- script.js
+++ script.js
@@ -9,9 +9,47 @@ async function gravatarHash(email) {
   return hash;
 }
 
-(async () => {
-  const hash = await gravatarHash("benjamin@soluml.com");
-  const imgSrc = "https://gravatar.com/avatar/" + hash;
+async function getGravatarAndColor(
+  email,
+  gravatarSize = 100,
+  dropPosition = { x: 50, y: 50 }
+) {
+  const { promise, resolve } = Promise.withResolvers();
 
-  console.log({ imgSrc });
+  const hash = await gravatarHash(email);
+
+  const canvasEl = document.createElement("canvas");
+  const ctx = canvasEl.getContext("2d");
+  canvasEl.width = gravatarSize;
+  canvasEl.height = gravatarSize;
+
+  const imgSrc = "https://gravatar.com/avatar/" + hash;
+  const imgEl = new Image();
+  imgEl.crossOrigin = "Anonymous";
+  imgEl.addEventListener(
+    "load",
+    () => {
+      ctx.drawImage(imgEl, 0, 0);
+
+      const imageData = ctx.getImageData(
+        dropPosition.x,
+        dropPosition.y,
+        gravatarSize,
+        gravatarSize
+      ).data;
+      const rgbColor = [imageData[0], imageData[1], imageData[2]];
+
+      resolve({ rgbColor, imgSrc });
+    },
+    { once: true }
+  );
+  imgEl.src = `${imgSrc}?s=${gravatarSize}`;
+
+  return promise;
+}
+
+(async () => {
+  const { rgbColor, imgSrc } = await getGravatarAndColor("benjamin@soluml.com");
+
+  console.log({ rgbColor, imgSrc });
 })();
